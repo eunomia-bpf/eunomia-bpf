36e428b2815553d59e6506d3ae78c74e331e3e35
yunwei37 <1067852565@qq.com>
2022-08-31 05:38:25 -0700

    add wrapper for C api in rust

--- eunomia-sdks/eunomia-rs/README.md
+++ eunomia-sdks/eunomia-rs/README.md
@@ -1,1 +1,21 @@
 # A rust binding for eunomia-bpf
+
+eunomia-bpf: https://github.com/eunomia-bpf/eunomia-bpf
+
+
+## Our target: Run <abbr title="Compile Once - Run Everywhere">CO-RE</abbr> eBPF function as a service!
+
+- Run `CO-RE` eBPF code without provisioning or managing infrastructure
+- simply requests with a json and run `any` pre-compiled ebpf code on `any` kernel version
+- very small and simple! Only a binary about `3MB`
+- as fast as `100ms` to load and run a ebpf program
+- `Distributed` and `decentralized`, No compile helper server
+
+In general, we develop an approach to compile, transmit, and run most libbpf CO-RE objects with some user space config meta data to help us load and operator the eBPF byte code.
+
+So, the only thing you need to do is focus on writing a single eBPF program in the kernel. We have a compiler here: [eunomia-cc](https://github.com/eunomia-bpf/eunomia-cc)
+
+
+## Build
+
+You will nedd to build the [eunomia-bpf](https://github.com/eunomia-bpf/eunomia-bpf) as a library first, then you can use it in your program.
--- eunomia-sdks/eunomia-rs/src/lib.rs
+++ eunomia-sdks/eunomia-rs/src/lib.rs
@@ -1,16 +1,76 @@
+//! # A rust binding for eunomia-bpf
+//!
+//! develop an approach to compile, transmit, and run most
+//! libbpf CO-RE objects with some user space config meta
+//! data to help us load and operator the eBPF byte code.
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+use std::error::Error;
+
 extern crate link_cplusplus;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+/// The eunomia ebpf rust bindings.
+pub struct Eunomia_bpf_program {
+    ctx: *mut eunomia_bpf,
+}
+
+impl Eunomia_bpf_program {
+    /// create a new eunomia bpf program from a json file
+    pub fn create_ebpf_program(json_data: &str) -> Result<Eunomia_bpf_program, &str> {
+        let ctx =
+            unsafe { create_ebpf_program_from_json(json_data.as_bytes().as_ptr() as *const i8) };
+        if ctx.is_null() {
+            return Err("Failed to create ebpf program");
+        } else {
+            Ok(Eunomia_bpf_program { ctx })
+        }
+    }
+    /// start running the ebpf program
+    ///
+    /// load and attach the ebpf program to the kernel to run the ebpf program
+    /// if the ebpf program has maps to export to user space, you need to call
+    /// the wait and export.
+    pub fn run(&self) -> Result<(), &str> {
+        let ret = unsafe { run_ebpf_program(self.ctx) };
+        if ret == 0 {
+            Ok(())
+        } else {
+            Err("Failed to run ebpf program")
+        }
+    }
+    /// wait for the program to exit and receive data from export maps
+    ///
+    /// if the program has a ring buffer or perf event to export data
+    /// to user space, the program will help load the map info and poll the
+    /// events automatically.
+    pub fn wait_and_export(&self) -> Result<(), &str> {
+        let ret = unsafe { wait_and_export_ebpf_program(self.ctx) };
+        if ret == 0 {
+            Ok(())
+        } else {
+            Err("Failed to wait and export ebpf program")
+        }
+    }
+    /// stop, detach, and clean up memory
+    ///
+    /// This is thread safe with wait_and_export.
+    /// it will notify the wait_and_export to exit and
+    /// wait until it exits.
+    pub fn stop(&self) {
+        unsafe { stop_and_clean_ebpf_program(self.ctx) };
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    #[expected_failure]
     fn test_run_ebpf_program() {
         unsafe {
             let input =
