0557c9469233862af51da0a84811487f1733aa87
yunwei37 <1067852565@qq.com>
2022-10-28 19:12:23 +0800

    eunomia-cc: remove more unnecessary files

--- eunomia-cc/cmd/src/export_template.c
+++
@@ -1,8 +0,0 @@
-// do not use this file: auto generated
-#include <stddef.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include "asm-generic/int-ll64.h"
-
-// make the compiler not ignore event struct
-// generate BTF event struct
