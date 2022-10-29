c417b894adaafa8fec7d097243f4d23f2d166e6b
yunwei37 <1067852565@qq.com>
2022-10-30 02:14:01 +0800

    eunomia-cc: add option to generate package.json

--- eunomia-cc/cmd/src/main.rs
+++ eunomia-cc/cmd/src/main.rs
@@ -3,10 +3,14 @@ mod config;
 mod export_types;
 use anyhow::Result;
 use clap::Parser;
-use compile_bpf::compile_bpf;
+use compile_bpf::*;
 use config::Args;
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    compile_bpf(&args)
+    compile_bpf(&args)?;
+    if args.pack_object {
+        pack_object_in_json(&args)?;
+    }
+    Ok(())
 }
