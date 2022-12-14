4b94175ac57b70b14187d77b32703bd45964ec8d
yunwei37 <1067852565@qq.com>
2022-12-15 00:59:08 +0800

    eunomia-bpf: introduce catch2 for unit test

--- eunomia-bpf/test/src/config_test.cpp
+++ eunomia-bpf/test/src/config_test.cpp
@@ -8,23 +8,18 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-
+#include <catch2/catch_test_macros.hpp>
 #include "eunomia/eunomia-meta.hpp"
 
 using namespace eunomia;
 
-int
-main(int argc, char **argv)
+TEST_CASE("test load config", "[eunomia_object_meta]")
 {
     std::ifstream condig_file("../../test/asserts/client.skel.json");
 
-    if (!condig_file.is_open()) {
-        std::cerr << "Failed to open json file" << std::endl;
-        return -1;
-    }
+    REQUIRE(condig_file.is_open());
     std::string json_str((std::istreambuf_iterator<char>(condig_file)),
                          std::istreambuf_iterator<char>());
     eunomia_object_meta meta;
     meta.from_json_str(json_str);
-    return 0;
 }
---
+++ eunomia-bpf/test/src/test_cpp_skel.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <fstream>
+#include <catch2/catch_test_macros.hpp>
+
+#include "eunomia/eunomia-bpf.hpp"
+
+using namespace eunomia;
+std::string
+read_file_data(const char *path)
+{
+    std::ifstream json_file(path);
+    std::string json_str((std::istreambuf_iterator<char>(json_file)),
+                         std::istreambuf_iterator<char>());
+    return json_str;
+}
+
+int
+test_create_and_stop()
+{
+    bpf_skeleton ebpf_program;
+    auto json_package = read_file_data("../../test/asserts/package.json");
+    REQUIRE(ebpf_program.open_from_json_config(json_package) == 0);
+    ebpf_program.destroy();
+    return 0;
+}
+
+int
+test_create_and_run()
+{
+    bpf_skeleton ebpf_program;
+    auto json_package = read_file_data("../../test/asserts/package.json");
+    REQUIRE(ebpf_program.open_from_json_config(json_package) == 0);
+    REQUIRE(ebpf_program.load_and_attach() == 0);
+    ebpf_program.destroy();
+    return 0;
+}
+
+int
+test_create_and_run_multi()
+{
+    bpf_skeleton ebpf_program;
+    auto json_package = read_file_data("../../test/asserts/package.json");
+    REQUIRE(ebpf_program.open_from_json_config(json_package) == 0);
+    REQUIRE(ebpf_program.load_and_attach() == 0);
+    bpf_skeleton ebpf_program2;
+    REQUIRE(ebpf_program2.open_from_json_config(json_package) == 0);
+    REQUIRE(ebpf_program2.load_and_attach() == 0);
+    ebpf_program.destroy();
+    ebpf_program2.destroy();
+    return 0;
+}
+
+TEST_CASE("creat and stop", "[creat]")
+{
+    REQUIRE(test_create_and_stop() == 0);
+}
+
+TEST_CASE("creat and run", "[creat]")
+{
+    REQUIRE(test_create_and_run() == 0);
+}
+
+TEST_CASE("creat and run multi", "[creat]")
+{
+    REQUIRE(test_create_and_run_multi() == 0);
+}
