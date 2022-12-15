e7055f40eabcee4d51d2f54b47efac1f7ed59366
yunwei37 <1067852565@qq.com>
2022-12-15 12:34:23 +0800

    eunomia-bpf: add minimal test and rename others

--- eunomia-bpf/test/src/config_test.cpp
+++ eunomia-bpf/test/src/config_test.cpp
@@ -9,17 +9,21 @@
 #include <iostream>
 #include <string>
 #include <catch2/catch_test_macros.hpp>
-#include "eunomia/eunomia-meta.hpp"
+#include "eunomia/eunomia-bpf.hpp"
+#include <json.hpp>
 
 using namespace eunomia;
+using json = nlohmann::json;
 
 TEST_CASE("test load config", "[eunomia_object_meta]")
 {
-    std::ifstream condig_file("../../test/asserts/client.skel.json");
-
+    std::ifstream condig_file("../../test/asserts/minimal.json");
     REQUIRE(condig_file.is_open());
-    std::string json_str((std::istreambuf_iterator<char>(condig_file)),
-                         std::istreambuf_iterator<char>());
-    eunomia_object_meta meta;
-    meta.from_json_str(json_str);
+    std::string json_package((std::istreambuf_iterator<char>(condig_file)),
+                             std::istreambuf_iterator<char>());
+    json config = json::parse(json_package);
+    bpf_skeleton ebpf_program;
+    REQUIRE(ebpf_program.open_from_json_config(json_package) == 0);
+    REQUIRE(ebpf_program.open_from_json_config(json_package) == 0);
+    REQUIRE(ebpf_program.open_from_json_config(config["meta"].dump(), {}) == 0);
 }
