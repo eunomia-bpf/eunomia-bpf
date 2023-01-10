6b53f9b8750cba6cfc74d0f74ce83bfabb1c231d
yang miao <1446174581@qq.com>
2023-01-10 18:23:36 +0800

    eunomia-bpf: Fix code quality issues

--- eunomia-sdks/eunomia-otel/build.rs
+++ eunomia-sdks/eunomia-otel/build.rs
@@ -39,7 +39,8 @@ fn main() {
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    static OUT_DIR: &str = "OUT_DIR";
+    let out_path = PathBuf::from(env::var(OUT_DIR).unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
