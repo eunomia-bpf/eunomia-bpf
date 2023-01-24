cc8bc6480da0e38f582ebf1f2b24641f86e75f33
Littlefisher619 <i@littlefisher.me>
2023-01-25 01:00:48 +0800

    example: fix build scripts

--- examples/bpftools/sigsnoop/build.sh
+++ examples/bpftools/sigsnoop/build.sh
@@ -6,8 +6,6 @@
 WAMR_DIR=${PWD}/../../../third_party/wasm-micro-runtime
 INCLUDE_DIR=${PWD}/eunomia-include/
 
-current_dir=$(pwd)
-
 OUT_FILE=sigsnoop.wasm
 
 # use WAMR SDK to build out the .wasm binary
@@ -15,12 +13,11 @@ OUT_FILE=sigsnoop.wasm
         --target=wasm32-wasi \
         -O0 -z stack-size=4096 -Wl,--initial-memory=65536 \
         --sysroot=/opt/wasi-sdk/share/wasi-sysroot  \
-        -I ${INCLUDE_DIR} \
-        -Wl,--allow-undefined-file=${WAMR_DIR}/wamr-sdk/app/libc-builtin-sysroot/share/defined-symbols.txt \
+        -I "${INCLUDE_DIR}" \
         -Wl,--export=all \
         -Wl,--export=bpf_main \
         -Wl,--export=process_event \
         -Wl,--strip-all,--no-entry \
         -Wl,--allow-undefined \
-        -o ${OUT_FILE} app.c
+        -o "${OUT_FILE}" app.c
 
