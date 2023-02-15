7bcea035f50293a0933b83b7bbb61fc1fcb2aa24
team317 <2629757717@qq.com>
2023-02-15 20:21:12 +0800

    fix handle_wasm

--- ecli/ecli-rs/build.rs
+++ ecli/ecli-rs/build.rs
@@ -13,7 +13,7 @@ fn main() {
     println!("cargo:rustc-link-search=../../bpf-loader/build/libbpf");
 
     println!("cargo:rustc-link-lib=static=eunomia");
-    println!("cargo:rustc-link-lib=static=wasm-bpf");
+    println!("cargo:rustc-link-lib=static=wasm-bpf_LIB");
     println!("cargo:rustc-link-lib=static=vmlib");
     println!("cargo:rustc-link-lib=static=stdc++");
     println!("cargo:rustc-link-lib=static=bpf");
