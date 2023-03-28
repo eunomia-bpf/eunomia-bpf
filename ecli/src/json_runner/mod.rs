3b8c1dddc7d815a47828bb0ba248d79632a8fad1
ocfox <i@ocfox.me>
2023-03-28 21:16:11 +0800

    disable bindgen code warning

--- ecli/src/json_runner/mod.rs
+++ ecli/src/json_runner/mod.rs
@@ -4,7 +4,10 @@
 //! All rights reserved.
 //!
 
+#[allow(dead_code)]
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
 mod eunomia_bpf;
-pub mod json;
 
+pub mod json;
 pub use eunomia_bpf::parse_args_to_json_config;
