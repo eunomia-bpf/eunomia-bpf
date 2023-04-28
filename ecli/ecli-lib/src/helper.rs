ddf95c316b65e6b4c24d404091038c4c622a399d
Officeyutong <yt.xyxx@gmail.com>
2023-04-28 19:27:28 +0800

    Apply changes from the reviewer
    Co-authored-by: ocfox <ocfox@outlook.com>

--- ecli/ecli-lib/src/helper.rs
+++ ecli/ecli-lib/src/helper.rs
@@ -5,19 +5,17 @@ const EUNOMIA_HOME_ENV: &str = "EUNOMIA_HOME";
 /// Get eunomia home directory
 pub fn get_eunomia_home() -> Result<String> {
     let eunomia_home = std::env::var(EUNOMIA_HOME_ENV);
-    match eunomia_home {
-        Ok(home) => Ok(home),
-        Err(_) => match home::home_dir() {
-            Some(home) => {
-                let home = home.join(".eunomia");
-                if !home.exists() {
-                    std::fs::create_dir_all(&home).map_err(Error::IOErr)?;
-                }
-                Ok(home.to_string_lossy().to_string())
-            }
-            None => Err(Error::Other(
-                "home dir not found. Please set EUNOMIA_HOME env.".to_string(),
-            )),
-        },
+    if let Ok(home) = eunomia_home {
+        Ok(home)
+    } else if let Some(home) = home::home_dir() {
+        let home = home.join(".eunomia");
+        if !home.exists() {
+            std::fs::create_dir_all(&home).map_err(Error::IOErr)?;
+        }
+        Ok(home.to_string_lossy().to_string())
+    } else {
+        Err(Error::Other(
+            "home dir not found. Please set EUNOMIA_HOME env.".to_string(),
+        ))
     }
 }
