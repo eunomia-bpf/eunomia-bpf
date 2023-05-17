be789e2da71a549482d55c83eb962ce47b487835
Officeyutong <yt.xyxx@gmail.com>
2023-05-17 16:32:44 +0800

    Try to Fix image issue (#235)
    * Try to Fix image issue
    
    * Try fix CI
    
    * Try fix CI
    
    * Try fix CI
    
    * Try fixing CI
    
    * Fix CI

--- compiler/cmd/build.rs
+++ compiler/cmd/build.rs
@@ -90,7 +90,8 @@ fn main() -> anyhow::Result<()> {
 
     let workspace_path = workdir().join("workspace");
 
-    fetch_and_build_bpftool()?;
+    fetch_and_build_bpftool()
+        .with_context(|| anyhow!("Failed to fetch or build the repo of bpftool"))?;
     fetch_git_repo(&vmlinux_repo(), &vmlinux_ref(), &vmlinux_repodir())
         .with_context(|| anyhow!("Failed to fetch vmlinux headers"))?;
     if workspace_path.exists() {
@@ -100,8 +101,10 @@ fn main() -> anyhow::Result<()> {
     }
     std::fs::create_dir_all(&workspace_path)
         .with_context(|| anyhow!("Failed to create workspace dir"))?;
-    std::fs::create_dir_all(workspace_path.join("bin"))?;
-    std::fs::create_dir_all(workspace_path.join("include"))?;
+    std::fs::create_dir_all(workspace_path.join("bin"))
+        .with_context(|| anyhow!("Failed to create `bin` directory of workspace"))?;
+    std::fs::create_dir_all(workspace_path.join("include"))
+        .with_context(|| anyhow!("Failed to create `include` directory of workspace"))?;
     let bpftool_repo_dir = workdir().join(bpftool_repodir());
     std::fs::copy(
         bpftool_repo_dir.join("src/bpftool"),
@@ -115,7 +118,8 @@ fn main() -> anyhow::Result<()> {
     )
     .with_context(|| anyhow!("Failed to copy libbpf headers"))?;
     // Avoid copying the .git folder
-    std::fs::remove_dir_all(workdir().join(vmlinux_repodir()).join(".git"))?;
+    std::fs::remove_dir_all(workdir().join(vmlinux_repodir()).join(".git"))
+        .with_context(|| anyhow!("Failed to remove .git directory "))?;
     fs_extra::dir::copy(
         workdir().join(vmlinux_repodir()),
         workspace_path.join("include"),
