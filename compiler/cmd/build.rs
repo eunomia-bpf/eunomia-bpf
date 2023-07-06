dd2806e89ba268f2bbbb2c280ca994cfcd1d59ee
Officeyutong <yt.xyxx@gmail.com>
2023-07-06 15:39:31 +0700

    Use cross-build to build aarch64 docker image (#277)
    * ecc: Allow using custom workspace
    
    * Update build.rs
    
    * Initial commit for cross-compile ecc
    
    * Fix
    
    Some work
    
    Update dockerfiles
    
    Fix workflow
    
    Fix dockerfile
    
    Fix dockerfile
    
    Fix workflow
    
    Fix workflow
    
    Fix workflow
    
    Fix dockerfile
    
    Fix dockerfile
    
    Fix dockerfile
    
    Fix dockerfile
    
    Fix dockerfile
    
    Fix build.rs
    
    Update
    
    Fix
    
    * Fix
    
    Fix workflow
    
    Fix workflow
    
    Fix workflow
    
    Fix dockerfile
    
    Fix dockerfile
    
    * Fix code analysising CI
    
    * Fix buildling of arm
    
    * Update build configuration
    
    * Fix build steps
    
    * Update dockerfile
    
    * Fix ecc build
    
    * Add comment
    
    * Fix workflow
    
    * Fix dockerfile
    
    * Fix docker image
    
    * Fix
    
    * Fix document check
    
    * Fix document

--- compiler/cmd/build.rs
+++ compiler/cmd/build.rs
@@ -10,7 +10,8 @@ use std::{
 };
 
 use anyhow::{anyhow, bail, Context};
-use fs_extra::dir::CopyOptions;
+use dircpy::copy_dir;
+use dircpy::CopyBuilder;
 
 const DEFAULT_BPFTOOL_REPO: &str = "https://github.com/eunomia-bpf/bpftool";
 const DEFAULT_BPFTOOL_REF: &str = "0594034";
@@ -93,13 +94,10 @@ fn main() -> anyhow::Result<()> {
     println!("cargo:rerun-if-env-changed=BPFTOOL_REF");
     println!("cargo:rerun-if-env-changed=VMLINUX_REPO");
     println!("cargo:rerun-if-env-changed=VMLINUX_REF");
+    println!("cargo:rerun-if-env-changed=ECC_CUSTOM_WORKSPACE_ROOT");
 
     let workspace_path = workdir().join("workspace");
 
-    fetch_and_build_bpftool()
-        .with_context(|| anyhow!("Failed to fetch or build the repo of bpftool"))?;
-    fetch_git_repo(&vmlinux_repo(), &vmlinux_ref(), &vmlinux_repodir())
-        .with_context(|| anyhow!("Failed to fetch vmlinux headers"))?;
     if workspace_path.exists() {
         println!("Removing existsing workspace..");
         std::fs::remove_dir_all(&workspace_path)
@@ -107,6 +105,17 @@ fn main() -> anyhow::Result<()> {
     }
     std::fs::create_dir_all(&workspace_path)
         .with_context(|| anyhow!("Failed to create workspace dir"))?;
+
+    if let Ok(v) = std::env::var("ECC_CUSTOM_WORKSPACE_ROOT") {
+        println!("Copying custom workspace from {}", v);
+        copy_dir(v, workspace_path).with_context(|| anyhow!("Failed to copy custom workspace"))?;
+        return Ok(());
+    }
+    fetch_and_build_bpftool()
+        .with_context(|| anyhow!("Failed to fetch or build the repo of bpftool"))?;
+    fetch_git_repo(&vmlinux_repo(), &vmlinux_ref(), &vmlinux_repodir())
+        .with_context(|| anyhow!("Failed to fetch vmlinux headers"))?;
+
     std::fs::create_dir_all(workspace_path.join("bin"))
         .with_context(|| anyhow!("Failed to create `bin` directory of workspace"))?;
     std::fs::create_dir_all(workspace_path.join("include"))
@@ -117,20 +126,19 @@ fn main() -> anyhow::Result<()> {
         workspace_path.join("bin/bpftool"),
     )
     .with_context(|| anyhow!("Failed to copy bpftool binary"))?;
-    fs_extra::dir::copy(
+    copy_dir(
         bpftool_repo_dir.join("src/libbpf/include/bpf"),
-        workspace_path.join("include"),
-        &CopyOptions::default(),
+        workspace_path.join("include/bpf"),
     )
     .with_context(|| anyhow!("Failed to copy libbpf headers"))?;
     // Avoid copying the .git folder
     std::fs::remove_dir_all(workdir().join(vmlinux_repodir()).join(".git"))
         .with_context(|| anyhow!("Failed to remove .git directory "))?;
-    fs_extra::dir::copy(
+    CopyBuilder::new(
         workdir().join(vmlinux_repodir()),
-        workspace_path.join("include"),
-        &CopyOptions::default().copy_inside(true),
+        workspace_path.join("include/vmlinux"),
     )
+    .run()
     .with_context(|| anyhow!("Failed to copy vmlinux headers"))?;
     Ok(())
 }
