4b5b5da8615919033c47847a6e5ea2b5004e51fd
Officeyutong <yt.xyxx@gmail.com>
2023-07-11 17:37:59 +0700

    Add workflow to release aarch64 ecc binary (#281)
    * Initial commit
    
    * stage
    
    * Build libbpf in vm
    
    * Fix
    
    * Update
    
    * fix
    
    * Install libelf and zlib1g on the host
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Add matrix
    
    * Fix
    
    * Fix
    
    * Merge configuations
    
    * Give it a better name
    
    * fix
    
    * Fixed now
    
    * Fix
    
    * Fix
    
    * Update ecli-binary.yaml

--- .github/scripts/copy-libs.sh
+++ .github/scripts/copy-libs.sh
@@ -1,11 +1,8 @@
-LD_DEBUG=libs /data/ecc 2> ld_debug_output
-echo "ecc running done"
+LD_DEBUG=libs $@ 2> ld_debug_output
 library_paths=$(awk '/^.+calling init:/{print $4}' ld_debug_output)
-echo "raw library paths: $library_paths"
 for library_path in $library_paths; do
     target_path="/data/libs${library_path}"
     target_dir=$(dirname "$target_path")
     mkdir -p "$target_dir"
-    echo "Copy $library_path"
     cp "$library_path" "$target_path"
 done
