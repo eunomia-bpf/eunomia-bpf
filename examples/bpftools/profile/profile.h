ddbd9353203255d4272817dcf8fd48417f9cb5de
yunwei37 <1067852565@qq.com>
2023-08-07 17:36:20 +0000

    Committing clang-format changes

--- examples/bpftools/profile/profile.h
+++ examples/bpftools/profile/profile.h
@@ -14,13 +14,13 @@
 typedef __u64 stack_trace_t[MAX_STACK_DEPTH];
 
 struct stacktrace_event {
-	__u32 pid;
-	__u32 cpu_id;
-	char comm[TASK_COMM_LEN];
-	__s32 kstack_sz;
-	__s32 ustack_sz;
-	stack_trace_t kstack;
-	stack_trace_t ustack;
+    __u32 pid;
+    __u32 cpu_id;
+    char comm[TASK_COMM_LEN];
+    __s32 kstack_sz;
+    __s32 ustack_sz;
+    stack_trace_t kstack;
+    stack_trace_t ustack;
 };
 
 #endif /* __PROFILE_H_ */
