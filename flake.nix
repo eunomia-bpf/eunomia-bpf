5f7ce05b95abebdcae0162b9fd51fca4c737ef85
oluceps <i@oluceps.uk>
2023-10-02 06:46:16 +0800

    flake: enhancements build fixes
    flake: complete eunomia-bpf reproducible build
    
    This commit brings:
    - Fix issue mentioned in 158
    - Update way of building ecli to adapt code changes
    - Add dev env with full eunomia-bpf toolchain
    
    flake: add pre-commit-hooks
    
    This commit adds pre commit check based on nix flake.
    
    chore: apply suggestions from flake check
    
    - Apply format suggestion from black
    - Rename file with space-separated names, which
      is not recommend on unix system
    
    flake: add wasm-bpf cli to dev env
    
    flake: devShell subdivision
    
    flake: sync bpftool to wasm-bpftool branch
    
    doc: fix display error in README
    
    flake: fetch vmlinux
    
    chore: update deprecated lock
    
    flake: adapt code changes on master
    
    flake: add missing include
    
    flake: add apps
    
    fix: cargo lock mismatch
    
    Previous version of Cargo.lock show `error: no
    matching package named bpf-oci found`.
    
    fix: nix build ecli
    
    flake: fix bpftool version

--- flake.nix
+++ flake.nix
@@ -1,117 +1,250 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-  inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    wasm-bpf.url = "github:eunomia-bpf/wasm-bpf";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem (with flake-utils.lib.system; [ x86_64-linux aarch64-linux ]) (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        version = pkgs.lib.substring 0 8 self.lastModifiedDate or self.lastModified or "19700101";
-        meta = with pkgs.lib; {
-          description = "A tool for push, pull and run pre-compiled eBPF programs as OCI images in Wasm module";
-          homepage = "https://eunomia.dev/";
-          license = licenses.mit;
-          maintainers = with maintainers; [ undefined-moe ];
-        };
-        vmlinux = pkgs.fetchFromGitHub {
-          owner = "eunomia-bpf";
-          repo = "vmlinux";
-          rev = "933f83becb45f5586ed5fd089e60d382aeefb409";
-          hash = "sha256-CVEmKkzdFNLKCbcbeSIoM5QjYVLQglpz6gy7+ZFPgCY=";
-        };
-        ecli = pkgs.stdenv.mkDerivation rec {
-          name = "ecli";
-          inherit version;
-          src = self;
+  outputs = { self, flake-utils, nixpkgs, pre-commit-hooks, wasm-bpf }:
+    flake-utils.lib.eachSystem
+      (with flake-utils.lib.system; [ x86_64-linux aarch64-linux ])
+      (system:
+        let
+          version = pkgs.lib.substring 0 8 self.lastModifiedDate or self.lastModified or "19700101";
+          meta = with pkgs.lib; {
+            description = "A tool for push, pull and run pre-compiled eBPF programs as OCI images in Wasm module";
+            homepage = "https://eunomia.dev/";
+            license = licenses.mit;
+            maintainers = with maintainers; [ undefined-moe ];
+          };
+          pkgs = import nixpkgs
+            {
+              inherit system;
+              overlays = [
+                (
+                  final: prev: {
+                    # using patched bpftool
+                    bpftool =
+                      prev.stdenv.mkDerivation {
+                        pname = "bpftool";
+                        version = "eunomia-edition-20230311";
+                        src = prev.fetchFromGitHub {
+                          owner = "eunomia-bpf";
+                          repo = "bpftool";
+                          rev = "05940344f5db18d0cb1bc1c42e628f132bc93123";
+                          sha256 = "sha256-g2gjixfuGwVnFlqCMGLWVPbtKOSpQI+vZwIZciXFPTc=";
+                          fetchSubmodules = true;
+                        };
 
-          cargoRoot = "ecli";
-          cargoDeps = pkgs.rustPlatform.importCargoLock {
-            lockFile = ./${cargoRoot}/Cargo.lock;
+                        buildInputs = with prev; [ llvmPackages_15.clang elfutils zlib llvmPackages_15.libllvm.dev ];
+
+                        buildPhase = ''
+                          make -C src
+                        '';
+
+                        installPhase = ''
+                          mkdir -p $out/include/bpf
+                          cp libbpf/src/* $out/include/bpf
+                          mkdir -p $out/bin
+                          cp src/bpftool $out/bin
+                        '';
+                      };
+                  }
+                )
+              ];
+            };
+          vmlinux =
+            with pkgs;(stdenv.mkDerivation
+              {
+                pname = "vmlinux";
+                version = "eunomia-edition-20230514";
+
+                src =
+                  pkgs.fetchFromGitHub {
+                    owner = "eunomia-bpf";
+                    repo = "vmlinux";
+                    rev = "933f83becb45f5586ed5fd089e60d382aeefb409";
+                    sha256 = "sha256-CVEmKkzdFNLKCbcbeSIoM5QjYVLQglpz6gy7+ZFPgCY=";
+                  };
+
+                installPhase = ''
+                  runHook preInstall
+                  cp -r $src $out
+                  runHook postInstall
+                '';
+
+              });
+
+          ecli = pkgs.stdenv.mkDerivation rec {
+            name = "ecli";
+            inherit version;
+            src = self;
+            cargoRoot = "ecli";
+            cargoDeps = pkgs.rustPlatform.importCargoLock {
+              lockFile = ./${cargoRoot}/Cargo.lock;
+            };
+
+            nativeBuildInputs = with pkgs;[
+              pkg-config
+            ]
+            ++
+            (with rustPlatform;
+            [
+              cargoSetupHook
+              cargo
+              rustc
+            ]);
+
+            buildInputs = with pkgs;[
+              zlib.static
+              elfutils
+              zlib
+              openssl.dev
+              llvmPackages.bintools
+            ];
+
+            preBuild = ''
+              cd ${cargoRoot}
+            '';
+
+            OPENSSL_NO_VENDOR = 1;
+
+            installPhase = ''
+              mkdir -p $out/bin
+              install -Dm 755 target/release/ecli-rs $out/bin/
+              install -Dm 755 target/release/ecli-server $out/bin/
+            '';
+            inherit meta;
+          };
+        in
+        rec {
+          packages = {
+            ecli = ecli;
+            ecli-rs = pkgs.stdenv.mkDerivation {
+              name = "ecli-rs";
+              inherit version;
+              src = ecli;
+              installPhase = ''
+                install -Dm 755 $src/bin/ecli-rs $out/bin/ecli-rs
+              '';
+              inherit meta;
+            };
+            ecli-server = pkgs.stdenv.mkDerivation {
+              name = "ecli-server";
+              inherit version;
+              src = ecli;
+              installPhase = ''
+                install -Dm 755 $src/bin/ecli-server $out/bin/ecli-server
+              '';
+              inherit meta;
+            };
+            ecc = pkgs.stdenv.mkDerivation rec{
+              pname = "ecc";
+
+              inherit version;
+              src = self;
+
+              cargoRoot = "compiler/cmd";
+
+              cargoDeps = pkgs.rustPlatform.importCargoLock {
+                lockFile = ./${cargoRoot}/Cargo.lock;
+              };
+
+              nativeBuildInputs = with pkgs;[
+                cmake
+                pkg-config
+              ]
+              ++
+              (with rustPlatform;
+              [
+                cargoSetupHook
+                bindgenHook
+                cargo
+                rustc
+              ]);
+
+              buildInputs = with pkgs;[
+                llvmPackages_latest.clang
+                elfutils
+                zlib
+              ];
+
+              dontUseCmakeConfigure = true;
+
+              preBuild = ''
+                rm compiler/cmd/build.rs # requires network access
+                export OUT_DIR=$(pwd)
+                mkdir -p $OUT_DIR/workspace/{include,bin}
+                cp -r ${vmlinux} $OUT_DIR/workspace/include/vmlinux
+                cp ${pkgs.bpftool}/bin/bpftool $OUT_DIR/workspace/bin
+                cp -r ${pkgs.bpftool}/include $OUT_DIR/workspace
+                cd ${cargoRoot}
+                cargo build --release
+              '';
+
+              postInstall = ''
+                mkdir -p $out/bin
+                install -Dm 755 target/release/ecc-rs $out/bin/
+              '';
+
+            };
+            bpftool = pkgs.bpftool;
           };
 
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            zlib.static
-            elfutils
-            zlib
-            openssl.dev
-            cargo
-            rustc
-            rustPlatform.cargoSetupHook
-            rustPlatform.bindgenHook
-          ];
+          devShells = rec {
 
-          preBuild = ''
-            cd ${cargoRoot}
-          '';
+            default = eunomia-dev;
 
-          installPhase = ''
-            mkdir -p $out/bin
-            install -Dm 755 target/release/ecli-rs $out/bin/
-            install -Dm 755 target/release/ecli-server $out/bin/
-          '';
-          inherit meta;
-        };
-      in
-      {
-        packages.ecli = ecli;
-        packages.ecli-rs = pkgs.stdenv.mkDerivation {
-          name = "ecli-rs";
-          inherit version;
-          src = ecli;
-          installPhase = ''
-            mkdir -p $out/bin
-            cp $src/bin/ecli-rs $out/bin/ecli-rs
-          '';
-          inherit meta;
-        };
-        packages.ecli-server = pkgs.stdenv.mkDerivation {
-          name = "ecli-server";
-          inherit version;
-          src = ecli;
-          installPhase = ''
-            mkdir -p $out/bin
-            cp $src/bin/ecli-server $out/bin/ecli-server
-          '';
-          inherit meta;
-        };
-        packages.ecc = pkgs.stdenv.mkDerivation rec {
-          name = "ecc";
-          inherit version;
-          src = self;
-          cargoRoot = "compiler/cmd";
-          cargoDeps = pkgs.rustPlatform.importCargoLock {
-            lockFile = ./${cargoRoot}/Cargo.lock;
+            eunomia-dev = pkgs.mkShell {
+              inputsFrom = with packages;
+                [ ecc ecli ] ++ (with pkgs;[ llvmPackages.bintools ]);
+
+            };
+
+            ebpf-dev = eunomia-dev // pkgs.mkShell {
+              buildInputs = (with pkgs; [ libbpf ])
+              ++ [ packages.ecli packages.ecc packages.ecli-server ]
+              ++ [ wasm-bpf.packages.${system}.default ];
+
+            };
           };
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            elfutils
-            zlib
-            python3
-            cargo
-            rustc
-            rustPlatform.cargoSetupHook
-            rustPlatform.bindgenHook
-          ];
-          buildInputs = with pkgs; [  ];
-          preBuild = ''
-            mkdir -p compiler/workspace/include
-            mkdir -p compiler/workspace/bin
-            ln -s ${vmlinux} compiler/workspace/include/vmlinux
-            cp ${pkgs.bpftool}/bin/bpftool compiler/workspace/bin
-          '';
-          dontMake = true;
-          buildPhase = ''
-            cd compiler/cmd
-            export OUT_DIR=.
-            cargo build --release
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            chmod +x target/release/ecc-rs
-            cp target/release/ecc-rs $out/bin/ecc
-          '';
-        };
-      }
-    );
+
+          apps = {
+            ecc = {
+              type = "app";
+              program = "${self.packages.${system}.ecc}/bin/ecc-rs";
+            };
+            ecli-rs = {
+              type = "app";
+              program = "${self.packages.${system}.ecli}/bin/ecli-rs";
+            };
+            ecli-server = {
+              type = "app";
+              program = "${self.packages.${system}.ecli}/bin/ecli-server";
+            };
+
+          };
+
+          checks = with pkgs; {
+            pre-commit-check =
+              pre-commit-hooks.lib.${system}.run
+                {
+                  src = lib.cleanSource ./.;
+                  hooks = lib.genAttrs
+                    [ "shellcheck" "black" "mdsh" ]
+                    (n: { enable = true; });
+                };
+          };
+        }) // {
+      overlays.default =
+        final: prev:
+        (prev.lib.genAttrs
+          (prev.lib.attrNames (self.packages))
+          (n: self.packages.${n}));
+
+    };
 }
